f58002a429716e257a52bb37b46e1571105c7eca
Hades1225-design <chang.h.y1225@gmail.com>
2025-08-20 00:31:14 +0800

    Update script.js

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ document.addEventListener("DOMContentLoaded", () => {
         // 隨機設定 CSS 變數
         dot.style.setProperty("--size", `${Math.random() * 6 + 3}px`);
         dot.style.setProperty("--rise", `${Math.random() * 10 + 12}s`);
-        dot.style.setProperty("--sway", `${Math.random() * 6 + 6}s`);
+        dot.style.setProperty("--sway", `${Math.random() * 6 + 2}s`);
         dot.style.setProperty("--flicker", `${Math.random() * 3 + 2}s`);
 
         bg.appendChild(dot);
